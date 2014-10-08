4ce2cd55299759e3d950266f5bd9155cba5fdf3e
Scott Banwart <sbanwart@stonedonut.com>
2014-10-08 09:48:44 -0400

    Script now writes the processed tasks to the Todo.txt file. Closes #2

--- recur.fsx
+++ recur.fsx
@@ -7,7 +7,8 @@
 open System
 open System.IO
 
-let inputFile = @"C:\Work\todo.txt-extras\recur.txt"
+let recurFile = @"C:\Work\todo.txt-extras\recur.txt"
+let todoFile = @"C:\Work\todo.txt-extras\todo.txt"
 let delimiter = " "
 
 match fsi.CommandLineArgs with
@@ -23,8 +24,7 @@ match fsi.CommandLineArgs with
         let day = input.Substring(0, 1).ToUpper() + input.Substring(1).ToLower()
         Enum.Parse(typeof<DayOfWeek>, day) :?> DayOfWeek
 
-    let processDateMacro (input : string) =
-        let macrostart = input.IndexOf("%date")
+    let rec processDateMacro (input : string, macrostart : int) = 
         if macrostart > 0 then
             let macroend = input.IndexOf("%", macrostart + 1)
             let macro = input.Substring(macrostart, (macroend - macrostart) + 1)
@@ -32,16 +32,23 @@ match fsi.CommandLineArgs with
             let operator = input.Substring(macrostart + 5, 1)
             if operator = "+" then
                 let additionalDays = Double.Parse(input.Substring(input.IndexOf(operator, macrostart) + 1, macroend - macrostart - 6))
-                input.Replace(macro, DateTime.Now.AddDays(additionalDays).ToString("yyyy-MM-dd"))
+                let newTask = input.Replace(macro, DateTime.Now.AddDays(additionalDays).ToString("yyyy-MM-dd"))
+                let nextMacro = newTask.IndexOf("%date")
+                processDateMacro(newTask, nextMacro)
             else
-                input.Replace(macro, DateTime.Now.ToString("yyyy-MM-dd"))
+                let newTask = input.Replace(macro, DateTime.Now.ToString("yyyy-MM-dd"))
+                let nextMacro = newTask.IndexOf("%date")
+                processDateMacro(newTask, nextMacro)
         else
             input
         
     let getLine =
-        let lines = fileReader inputFile
+        let lines = fileReader recurFile
         lines
-        |> Seq.map (fun line -> line.Substring(0, line.IndexOf(delimiter)), processDateMacro(line.Substring(line.IndexOf(delimiter) + 1)))
+        |> Seq.map (fun line ->
+            let task = line.Substring(line.IndexOf(delimiter) + 1)
+            let macrostart = task.IndexOf("%date")
+            line.Substring(0, line.IndexOf(delimiter)), processDateMacro(task, macrostart))
         |> Seq.choose (fun t ->
             match t with
             | (f, r) when f = "daily" -> Some(r)
@@ -54,4 +61,7 @@ match fsi.CommandLineArgs with
     let tasks = getLine
 
     tasks
-    |> Seq.iter (fun t -> printfn "Description: %s" t)
+    |> Seq.iter (fun t -> 
+        use fileWriter = new StreamWriter(File.Open(todoFile, FileMode.Append))
+        fileWriter.WriteLine(t))
+                 
